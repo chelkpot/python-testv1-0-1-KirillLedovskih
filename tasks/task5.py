902933c9c2c5d16e9f0a3dab20acd55f54259447
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 <mcmi17@mail.com>
2025-12-09 17:09:56 +0300

    d

--- tasks/task5.py
+++ tasks/task5.py
@@ -4,7 +4,9 @@
 
 def solve():
 # Ниже пишите решение задачии(Обязательно поставьте четыре пробела после функции Solve())
-
+    numbers = list(map(int, input("Введите числа через пробел: ").split()))
+    squares = list(map(lambda x: x ** 2, numbers))
+    print("Результат:", *squares)
 
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
